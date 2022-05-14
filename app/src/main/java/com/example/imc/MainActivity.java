97f237381fd6a2e3ececd2c0a8cf2a6928d31961
Dilso-Lopes <sniper_neon@hotmail.com>
2022-05-14 15:51:39 -0300

    Atualização de variáveis

--- app/src/main/java/com/example/imc/MainActivity.java
+++ app/src/main/java/com/example/imc/MainActivity.java
@@ -13,71 +13,73 @@ import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText editText_alt, editText_peso, editText_res;
-    private Button btn_cal;
-    private double us_alt, us_peso;
+    private EditText editText_altura, editText_peso, editText_resultado;
+    private Button button_calcular;
+    private double usuario_altura, usuario_peso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText_alt = findViewById(R.id.text_altura);
+        editText_altura = findViewById(R.id.text_altura);
         editText_peso = findViewById(R.id.text_peso);
-        editText_res = findViewById(R.id.text_res);
-        btn_cal = findViewById(R.id.button_cal);
+        editText_resultado = findViewById(R.id.text_resultado);
+        button_calcular = findViewById(R.id.button_calculo);
 
         maskEdit();
 
-        btn_cal.setOnClickListener(new View.OnClickListener() {
+        button_calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(editText_alt.getText().toString().isEmpty() || editText_peso.getText().toString().isEmpty()) {
+                if(editText_altura.getText().toString().isEmpty() || editText_peso.getText().toString().isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Favor preencher os campos em branco!!", Toast.LENGTH_SHORT).show();
 
                 }else{
-                    us_alt = Double.parseDouble(editText_alt.getText().toString());
-                    us_peso = Double.parseDouble(editText_peso.getText().toString());
-                    CalculoImc(us_alt, us_peso);
-                    editText_alt.setText("");
+                    usuario_altura = Double.parseDouble(editText_altura.getText().toString());
+                    usuario_peso = Double.parseDouble(editText_peso.getText().toString());
+                    CalculoImc(usuario_altura, usuario_peso);
+                    editText_altura.setText("");
                     editText_peso.setText("");
+
                 }
+
             }
         });
+
     }
     private void CalculoImc(Double altura, Double peso){
         double media = 0.0;
         double res_altura = 0.0;
         res_altura = Math.pow(altura, 2);
         media = peso/res_altura;
-        Log.i("CalculoImc", String.valueOf(media));
 
         if(media<18.5){
-            editText_res.setText("Abaixo do peso!!");
+            editText_resultado.setText("Abaixo do peso!!");
 
         }else if(media<=24.9){
-            editText_res.setText("Peso normal!!");
+            editText_resultado.setText("Peso normal!!");
 
         }else if(media<=29.9){
-            editText_res.setText("Acima do peso!!");
+            editText_resultado.setText("Acima do peso!!");
 
         }else if(media<=34.9){
-            editText_res.setText("Obesidade Grau I!!");
+            editText_resultado.setText("Obesidade Grau I!!");
 
         }
         else if(media<=39.9){
-            editText_res.setText("Obesidade Grau II!!");
+            editText_resultado.setText("Obesidade Grau II!!");
 
         }else{
-            editText_res.setText("Obesidade Grau III!!");
+            editText_resultado.setText("Obesidade Grau III!!");
 
         }
+
     }
-    //Método para adicionar máscaras em EditText definidos
     private void maskEdit() {
         SimpleMaskFormatter mskTel = new SimpleMaskFormatter("N.NN");
-        MaskTextWatcher maskT = new MaskTextWatcher(editText_alt, mskTel);
-        editText_alt.addTextChangedListener(maskT);
-
+        MaskTextWatcher maskT = new MaskTextWatcher(editText_altura, mskTel);
+        editText_altura.addTextChangedListener(maskT);
 
     }
+
 }
