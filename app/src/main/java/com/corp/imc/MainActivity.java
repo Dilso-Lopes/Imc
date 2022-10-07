f9f640b99bfa889db0da14661261a13622157c85
Dilso-Lopes <sniper_neon@hotmail.com>
2022-10-07 12:43:42 -0300

    Modificação de layout e acrescentado mais uma função.

--- app/src/main/java/com/corp/imc/MainActivity.java
+++ app/src/main/java/com/corp/imc/MainActivity.java
@@ -12,8 +12,10 @@ import com.corp.imc.R;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText editText_altura, editText_peso, editText_resultado;
+    private EditText editText_altura, editText_peso, editText_resultado, editText_ideal;
     private Button button_calcular;
     private double usuario_altura, usuario_peso;
 
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         editText_altura = findViewById(R.id.text_altura);
         editText_peso = findViewById(R.id.text_peso);
         editText_resultado = findViewById(R.id.text_resultado);
+        editText_ideal = findViewById(R.id.text_ideal);
         button_calcular = findViewById(R.id.button_calculo);
 
         maskEdit();
@@ -55,26 +58,43 @@ public class MainActivity extends AppCompatActivity {
 
         if(media<18.5){
             editText_resultado.setText("Abaixo do peso!!");
+            calcularPesoIdeal();
 
         }else if(media<=24.9){
             editText_resultado.setText("Peso normal!!");
+            editText_ideal.setText("Peso ok!");
 
         }else if(media<=29.9){
             editText_resultado.setText("Acima do peso!!");
+            calcularPesoIdeal();
 
         }else if(media<=34.9){
             editText_resultado.setText("Obesidade Grau I!!");
-
+            calcularPesoIdeal();
         }
         else if(media<=39.9){
             editText_resultado.setText("Obesidade Grau II!!");
+            calcularPesoIdeal();
 
         }else{
             editText_resultado.setText("Obesidade Grau III!!");
+            calcularPesoIdeal();
 
         }
 
     }
+
+    private void calcularPesoIdeal(){
+        usuario_altura = Double.parseDouble(editText_altura.getText().toString());
+        double peso_ideal = 0;
+        double res_altura = 0.0;
+        res_altura = Math.pow(usuario_altura, 2);
+        peso_ideal = (25*res_altura)-1;
+        String peso = new DecimalFormat("##").format(peso_ideal);
+        editText_ideal.setText(peso+"Kg");
+
+    }
+
     private void maskEdit() {
         SimpleMaskFormatter mskTel = new SimpleMaskFormatter("N.NN");
         MaskTextWatcher maskT = new MaskTextWatcher(editText_altura, mskTel);
